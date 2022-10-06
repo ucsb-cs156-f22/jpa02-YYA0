fcff36a2fb61a0ba1b5a706dba0201373f382b04
Yu <yu_yao@ucsb.edu>
2022-10-06 01:30:03 -0700

    <add ! to Hello Message>

--- src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetings from yu-yao";
+        return "Greetings from yu-yao!";
     }
     
 }
